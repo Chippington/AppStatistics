72c10a5155897f76ef1c940208a7998b5a25d93b
Chippington <scythix@hotmail.com>
2017-09-02 08:02:14 -0700

    Revert "Migration to .NET Core 2.0, added 'Common' project"
    This reverts commit 3084d0e4712599353f1ca2c60bba6cb4e44e01f2.

---
+++ AppStatistics/Config.cs
@@ -0,0 +1,13 @@
+﻿using AppStatistics.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppStatistics
+{
+    public static class Config
+    {
+		public static IDataStore store = new TestDataStore();
+    }
+}
